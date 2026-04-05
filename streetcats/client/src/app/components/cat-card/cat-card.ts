15f3aa5ce690902825b292e80728f61e5922e238
michela <polliomichela360@gmail.com>
2026-04-05 18:16:20 +0200

    update

--- streetcats/client/src/app/components/cat-card/cat-card.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-cat-card',
-  imports: [],
-  templateUrl: './cat-card.html',
-  styleUrl: './cat-card.scss',
-})
-export class CatCard {}
